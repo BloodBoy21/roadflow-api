dcde061ba3b1732a8c07be1dda05a9d462075aa7
Alan B <death1027@outlook.com>
2025-06-11 23:46:41 -0600

    feat: :construction: delete input webhook

--- routes/api/v1/organization.py
+++ routes/api/v1/organization.py
@@ -31,3 +31,21 @@ async def create_webhook_input(
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) from e
+
+
+@organization_router.delete(
+    "/{org_id}/webhook/input/{webhook_id}",
+    status_code=204,
+)
+@validate_org_middleware
+async def delete_webhook_input(
+    org_id: int,
+    webhook_id: str,
+    user: UserRead = Depends(user_is_authenticated),
+):
+    try:
+        # Here you would typically process the data, e.g., update a database, trigger a build, etc.
+        # For now, we just log it
+        await repository.sql.input_webhook.delete({"id": webhook_id})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
