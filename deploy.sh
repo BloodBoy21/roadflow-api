892db24588b3f29fe0e3c60350b6025eb0ae882b
Alan B <death1027@outlook.com>
2025-06-21 21:17:08 -0600

    chore: :hammer: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
+
+REGION="us-west1"
+PROJECT_ID="manifest-grin-462423-m3"
+REPO_NAME="hackaton"
+
 docker buildx build --platform linux/amd64 -t bloodbloy/roadflow-api . --no-cache
 
- docker tag bloodbloy/roadflow-api northamerica-south1-docker.pkg.dev/manifest-grin-462423-m3/flow-images/flow-api:latest
+ docker tag bloodbloy/roadflow-api ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/flow-api:latest
 
- docker push  northamerica-south1-docker.pkg.dev/manifest-grin-462423-m3/flow-images/flow-api:latest
+ docker push  ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/flow-api:latest
 
- gcloud run deploy flow-api --image northamerica-south1-docker.pkg.dev/manifest-grin-462423-m3/flow-images/flow-api:latest --region northamerica-south1 --allow-unauthenticated --env-vars-file .env.yaml --memory 2Gi --cpu 2
+ gcloud run deploy flow-api --image ${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/flow-api:latest --region ${REGION} --allow-unauthenticated --env-vars-file .env.yaml --memory 2Gi --cpu 2
